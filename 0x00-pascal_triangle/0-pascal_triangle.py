d1a4863750dc8fd2d71631d5d7687497e04ebb57
Alexoat76 <3915@holbertonschool.com>
2022-07-26 14:04:49 -0500

    Function | Returns a list of lists of integers repr.| Pascal’s triangle :heavy_check_mark:

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -8,6 +8,7 @@ def pascal_triangle(n):
     '''
     Returns a list of lists of integers representing the Pascal's triangle of n
     '''
+
     if n <= 0:  # if n is not a positive integer
         return []
     triangle = [[1]]  # initialize the first row of the triangle
