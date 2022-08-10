f3f76bef1ca491249ae047f6ea34d90b37f923f7
Alexoat76 <3915@holbertonschool.com>
2022-08-10 17:32:18 -0500

    Test

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 
-# set recursion limit
-from sys import setrecursionlimit
-
 """
 This module contains a method that calculates the fewest number of operations
 needed to result in exactly n H characters in the file.
