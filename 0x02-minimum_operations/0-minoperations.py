73fee2c622f6c93b9a25da23ecaaca01ef8817c3
Alexoat76 <3915@holbertonschool.com>
2022-08-10 17:15:39 -0500

    Method | Calculates the fewest number of operations needed for n H characters in the file :heavy_check_mark:

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -11,26 +11,23 @@ Returns: integer
 if n is impossible to achieve, returns 0
 """
 
+def minOperations(n):
+    """
+    In a text file, there is a single character H.
+    Your text editor can execute only two operations in this file:
+    Copy All and Paste. Given a number n, write a method that calculates
+    the fewest number of operations needed to result in exactly n H characters
+    in the file
+    """
 
-# Function to calculate the fewest number of operations
-def minOperations(n):  # n is the number of H characters in the file
+    if n <= 1:
+        return 0
+    num, div, numOfOperations = n, 2, 0
 
-    setrecursionlimit(10**6)  # set recursion limit to 10^6
-
-    operations = 0  # number of operations given the current number of H char.
-    inc = 2   # 3 4 5 6 etc. are the possible values of n
-
-    # factor = 2
-    # no even division (prime) inc = n
-
-    if type(n) is not int or n < 2:
-        return 0  # n is not a valid input (not an integer or less than 2)
-
-    while n != 1:
-        if n % inc == 0:
-            operations += inc  # 1
-            n /= inc
-            inc = 1  # resets for next loop
-        inc = inc + 1
-
-    return operations  # number of operations needed to achieve n H characters
+    while num > 1:
+        if num % div == 0:
+            num = num / div
+            numOfOperations = numOfOperations + div
+        else:
+            div += 1
+    return numOfOperations
