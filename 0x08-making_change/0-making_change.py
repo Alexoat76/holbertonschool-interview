fb4f87d2ec9d4878e71408f73249f386fdba3cce
Alexoat76 <3915@holbertonschool.com>
2022-09-21 10:42:06 -0500

    Try to determine the fewest number of coins :heavy_check_mark:

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -6,20 +6,16 @@ coins needed to meet a given amount total
 
 
 def makeChange(coins, total):
-    # Returns fewest number of coins needed to meet total
     """Clasic Bottom-Up dynamic programming"""
-    tmp_value = 0  # temporary value to store the value of the coin being used
-    # sort the coins in descending order to make the algorithm more efficient
+    tmp_value = 0
     coins.sort(reverse=True)
 
-    if total < 0:  # if the total is negative, return -1 (no solution)
+    if total < 0:
         return 0
 
-    for coin in coins:  # iterate through the coins list
-        # if the remainder is less than the total amount of change
+    for coin in coins:
         if total % coin <= total:
-            # add the number of coins to the tmp_value variable
             tmp_value += total // coin
-            total = total % coin  # update the total amount of change
+            total = total % coin
 
-    return tmp_value if total == 0 else -1  # return the number of coins used
+    return tmp_value if total == 0 else -1
