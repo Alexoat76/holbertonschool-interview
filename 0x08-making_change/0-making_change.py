e036d82a36e23962890ae613993f97333dfd4aff
Alexoat76 <3915@holbertonschool.com>
2022-09-21 10:49:59 -0500

    Try to determine the fewest number of coins :heavy_check_mark:

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -21,11 +21,11 @@ def makeChange(coins, total):
         coins = sorted(coins, reverse=True)
         coin_dict = {}  # Create empty dictionary to store coin values
         while total is not None:  # While total is not None (empty)
-            for coin in coins:  # For each coin in coins list
-                if total % coin == 0:  # If total is divisible by coin value
-                    coin_dict[coin] = total / coin  # Add coin value to dictionary
+            for c in coins:  # For each coin in coins list
+                if total % c == 0:  # If total is divisible by coin value
+                    coin_dict[c] = total / c  # Add coin value to dictionary
                     return(int(sum(coin_dict.values())))  # Return sum of val.
                 else:
-                    coin_dict[coin] = trunc(total / float(coin))  # Add coin value
-                    total -= (coin * coin_dict[coin])  # Subt. coin value from total
+                    coin_dict[c] = trunc(total / float(c))  # Add coin value
+                    total -= (c * coin_dict[c])  # Subt. coin value from total
             return -1  # If total is not None, return -1
