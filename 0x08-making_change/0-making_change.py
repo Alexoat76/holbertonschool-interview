a9007cd9e35d55d77659663cc7b27a176d8d62d6
Alexoat76 <3915@holbertonschool.com>
2022-09-23 11:15:11 -0500

    Testing answer again

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -8,19 +8,18 @@ coins needed to meet a given amount total
 def makeChange(coins, total):
     """ Returns fewest number of coins needed to meet total
     """
-    if total <= 0:
+    total = [int(total)]
+    if total[0] <= 0:
         return 0
-    else:
-        if coins is None or len(coins) == 0:
-            return -1
-        else:
-            coins = sorted(coins, reverse=True)
-            count = 0
-            for i in coins:
-                if total >= i:
-                    count += total // i
-                    total = total % i
-            if total == 0:
-                return count
-            else:
-                return -1
+    coins.sort(reverse=True)
+    coins = [int(i) for i in coins]
+    coins = [i for i in coins if i <= total[0]]
+    if not coins:
+        return -1
+    count = 0
+    for i in coins:
+        count += total[0] // i
+        total[0] = total[0] % i
+        if total[0] == 0:
+            return count
+    return -1
