3f60169dd6fffdf1cba068240ce34879e49bb2cc
Alexoat76 <3915@holbertonschool.com>
2022-09-23 10:45:44 -0500

    Testing answer

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -14,14 +14,15 @@ def makeChange(coins, total):
         if coins is None or len(coins) == 0:
             return -1
         else:
-            coins.sort(reverse=True)
-            count = 0
-            for i in coins:
-                if total >= i:
-                    count += total // i
-                    total = total % i
-            if total != 0:
+            coins = sorted(coins, reverse=True)
+            coin_dict = {}
+            while total is not None:
+                for c in coins:
+                    if total % c == 0:
+                        coin_dict[c] = total / c
+                        return(int(sum(coin_dict.values())))
+                    else:
+                        coin_dict[c] = total // c
+                        total -= (c * coin_dict[c])
+
                 return -1
-            else:
-                return count
-                
