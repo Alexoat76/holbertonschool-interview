e7e6ed9d983f28a3c3cd9242f3d117dd7a9ca867
Alexoat76 <3915@holbertonschool.com>
2022-09-23 10:36:04 -0500

    Testing answer

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -8,24 +8,17 @@ coins needed to meet a given amount total
 def makeChange(coins, total):
     """ Returns fewest number of coins needed to meet total
     """
-
-    if total <= 0:  # If total is 0 or less, return 0
+    if total <= 0:
         return 0
-        # If total is greater than 0 and coins is empty, return -1 (impossible)
-    else:
-        from math import trunc
-        """ Import trunc function from math module to round down
-            to nearest integer
-        """
-        # Sort coins in descending order (largest to smallest)
-        coins = sorted(coins, reverse=True)
-        coin_dict = {}  # Create empty dictionary to store coin values
-        while total is not None:  # While total is not None (empty)
-            for c in coins:  # For each coin in coins list
-                if total % c == 0:  # If total is divisible by coin value
-                    coin_dict[c] = total / c  # Add coin value to dictionary
-                    return(int(sum(coin_dict.values())))  # Return sum of val.
-                else:
-                    coin_dict[c] = trunc(total / float(c))  # Add coin value
-                    total -= (c * coin_dict[c])  # Subt. coin value from total
-            return -1  # If total is not None, return -1
+    coins.sort(reverse=True)
+    num_coins = 0
+    for coin in coins:
+        if total == 0:
+            return num_coins
+        if coin <= total:
+            num_coins += total // coin
+            total = total % coin
+    if total > 0:
+        return -1
+    return num_coins
+    
