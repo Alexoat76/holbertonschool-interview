9a1888d79cb5e39ce8954c679b9f6df5bba75849
Alexoat76 <3915@holbertonschool.com>
2022-09-23 18:43:44 -0500

    Determine the fewest number of coins | No efficiency | :heavy_check_mark:

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -25,21 +25,12 @@ def makeChange(coins, total):
         # sort coins in ascending order (smallest to largest)
     coins = sorted(coins)
     # initialize dynamic programming list with inf values
-    dynamic = [float('inf')] * (total + 1)
-    # set first value to 0 (no coins needed to make 0 change)
-    dynamic[0] = 0
-    # iterate through each total value from 0 to total
-    for i in range(total + 1):
+    dynamic = [float('inf') for i in range(total + 1)]
+    dynamic[0] = 0  # base case: 0 coins needed to make 0 change
+    for i in range(len(dynamic)):
         for coin in coins:
-            if coin > i:  # if coin value is greater than total, break
-                break
-            # if previous total is possible to make change for (not -1)
-            if dynamic[i - coin] != -1:
-                """ set current total to min of previous total + 1
-                    coin or current total
-                """
-                dynamic[i] = min(dynamic[i - coin] + 1, dynamic[i])
-                # if total is still inf, return -1 (can't make change)
-    if dynamic[total] == float('inf'):
+            if i - coin >= 0:
+                dynamic[i] = min(dynamic[i], dynamic[i - coin] + 1)
+    if dynamic[-1] == float('inf'):
         return -1
-    return dynamic[total]  # return total number of coins needed to make change
+    return dynamic[-1]
