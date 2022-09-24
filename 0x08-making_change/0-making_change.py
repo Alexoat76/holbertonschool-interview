ab6929c03c67793b82af668ec11ade41caa77a54
Alexoat76 <3915@holbertonschool.com>
2022-09-23 19:02:04 -0500

    Determine the fewest number of coins | No efficiency | :heavy_check_mark:

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -4,22 +4,14 @@ Defines function that determines the fewest number of coins to make change
 """
 
 
-def makeChange(coins: int, total: int) -> int:
+def makeChange(coins: int, amount: int) -> int:
     """
-    Determines the fewest number of coins needed to meet a given total
-    parameters:
-        coins [list or positive ints]:
-            the values of the coins in your possession
-            you can assume you have an infinite number of coins of all values
-        total [int]:
-            total amount of change to make
-            if total is 0 or less, return 0
-    returns:
-        the fewest number of coins to make the change
-        or -1 if the total change cannot be made with the given coins
+    Returns the fewest number of coins to make change
     """
-    dynamic = [0] + [float('inf')] * total
+    dynamic = [float('inf') for _ in range(amount + 1)]
+    dynamic[0] = 0
     for coin in coins:
-        for i in range(coin, total + 1):
-            dynamic[i] = min(dynamic[i], dynamic[i - coin] + 1)
-    return dynamic[total] if dynamic[total] != float('inf') else -1
+        for i in range(len(dynamic)):
+            if i >= coin:
+                dynamic[i] = min(dynamic[i], dynamic[i - coin] + 1)
+    return dynamic[amount] if dynamic[amount] != float('inf') else -1
